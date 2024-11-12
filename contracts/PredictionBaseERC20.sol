50f3e623a2f9fc4689b5757c5f1ce701d421a8e4
markolazic01 <markolazic01@gmail.com>
2024-11-12 14:50:26 +0700

    :sparkles: feat: Base for predictions paid with erc20

--- contracts/PredictionBaseERC20.sol
+++ contracts/PredictionBaseERC20.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import {SafeERC20, IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {PredictionBase} from "./PredictionBase.sol";
 
-abstract contract PredictionBaseNative is PredictionBase {
+abstract contract PredictionBaseERC20 is PredictionBase {
     /**
      * @notice Bet bull position
      * @param epoch: epoch
      * @param bull: true -> bull / false -> bear
      */
-    function bet(uint256 epoch, bool bull) external payable whenNotPaused nonReentrant onlyEOA {
-        _bet(epoch, msg.value, bull);
+    function bet(uint256 epoch, uint256 amount, bool bull) external whenNotPaused nonReentrant onlyEOA {
+        _bet(epoch, amount, bull);
     }
 
     function _pay(address to, uint256 value) internal override {
-        (bool success,) = to.call{value: value}("");
-        if (!success) revert SeiTransferFailed();
+        SafeERC20.safeTransfer(token, to, value);
     }
 
     function _getPrice() internal override virtual returns (uint256) {
